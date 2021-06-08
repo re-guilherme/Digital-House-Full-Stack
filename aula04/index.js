8efd934b2cd47c0a4a0f0d0b72ef61bac50a88d0
Renato Guilherme <rg.mota10@gmail.com>
2021-06-07 21:53:07 -0300

    adicionando conteúdo referente a aula 04

---
+++ aula04/index.js
@@ -0,0 +1,35 @@
+let nome = 'Renato' ;
+let idade = 31 ;
+let eMaiorDeIdade = true ;
+
+let pessoa = {
+    nome : nome,
+    idade : idade ,
+    eMaiorDeIdade : eMaiorDeIdade
+}
+
+let animais = ['cachorro', 'tigre', 'águia', 'elefante'] ;
+
+let numero1 = 5 ;
+let numero2 = 7 ;
+
+let resultadoSoma = numero1 + numero2
+console.log('O resultado da soma entre 5 e 7 é: '+ resultadoSoma)
+
+let resultadoSubtracao = numero1 - numero2
+console.log('O resultado da subtração entre 5 e 7 é: ' + resultadoSubtracao)
+
+let resultadoDivisao = numero1 / numero2
+console.log('O resultado da divisão entre 5 e 7 é: ' + resultadoDivisao)
+
+let resultadoMultiplicacao = numero1 * numero2
+console.log('O resultado da multiplicação entre 5 e 7 é: ' + resultadoMultiplicacao)
+
+if(numero1 > numero2){
+    console.log(numero1 +'é maior que' + numero2)
+    }else{numero2 + 'é maior que' + numero1}
+    
+
+
+        
+    
